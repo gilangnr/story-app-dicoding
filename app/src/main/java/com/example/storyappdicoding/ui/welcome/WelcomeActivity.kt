d9e1d4f554b60f941ad63d387400f6654deef6f2
Gilang <gilangnurrizki173@gmail.com>
2024-10-30 09:02:31 +0700

    welcome ui

--- app/src/main/java/com/example/storyappdicoding/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/storyappdicoding/ui/welcome/WelcomeActivity.kt
@@ -1,14 +1,38 @@
 package com.example.storyappdicoding.ui.welcome
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.storyappdicoding.R
+import com.example.storyappdicoding.databinding.ActivityWelcomeBinding
+import com.example.storyappdicoding.ui.login.LoginActivity
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
