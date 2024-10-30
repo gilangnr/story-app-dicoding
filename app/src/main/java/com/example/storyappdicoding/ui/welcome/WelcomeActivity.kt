64abfdc1054ffd03c998fcae2f5e9fed50c91c7b
Gilang <gilangnurrizki173@gmail.com>
2024-10-30 07:51:29 +0700

    first commit

---
+++ app/src/main/java/com/example/storyappdicoding/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.storyappdicoding.ui.welcome
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.storyappdicoding.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
