2a085d4e35bcdc0680e7f4ef01549f790cc6021a
Gilang <gilangnurrizki173@gmail.com>
2024-10-30 11:59:20 +0700

    login ui

--- app/src/main/java/com/example/storyappdicoding/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/storyappdicoding/ui/login/LoginActivity.kt
@@ -1,21 +1,32 @@
 package com.example.storyappdicoding.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.storyappdicoding.R
+import com.example.storyappdicoding.databinding.ActivityLoginBinding
+import com.example.storyappdicoding.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.toRegister.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
