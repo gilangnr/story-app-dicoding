56048501f52de6cfea1847e558e19d2b86ad2569
Gilang <gilangnurrizki173@gmail.com>
2024-11-01 09:08:34 +0700

    add toolbar in main

--- app/src/main/java/com/example/storyappdicoding/MainActivity.kt
+++ app/src/main/java/com/example/storyappdicoding/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.storyappdicoding
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.storyappdicoding.databinding.ActivityMainBinding
 import com.example.storyappdicoding.pref.SessionManager
 import com.example.storyappdicoding.ui.login.LoginActivity
+import com.example.storyappdicoding.ui.welcome.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         sessionManager = SessionManager(this)
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -34,9 +40,35 @@ class MainActivity : AppCompatActivity() {
             binding.txtToken.text = "Welcome, your token is: $token"
         }
 
-        binding.btnLogout.setOnClickListener {
-            logout()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_logout -> {
+                showLogoutConfirmation()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun showLogoutConfirmation() {
+        AlertDialog.Builder(this)
+            .setTitle("Confirm Logout")
+            .setMessage("Are you sure you want to logout?")
+            .setPositiveButton("Yes") { dialog, _ ->
+                logout()
+                dialog.dismiss()
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
     private fun logout() {
@@ -45,7 +77,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun navigateToLogin() {
-        val intent = Intent(this, LoginActivity::class.java)
+        val intent = Intent(this, WelcomeActivity::class.java)
         startActivity(intent)
         finish()
     }
