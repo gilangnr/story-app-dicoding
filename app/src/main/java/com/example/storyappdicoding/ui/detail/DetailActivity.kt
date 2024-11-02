2e5628eb3c234bd29cc8d79abbf00a65e4976205
Gilang <gilangnurrizki173@gmail.com>
2024-11-02 10:40:41 +0700

    get id story from detail

--- app/src/main/java/com/example/storyappdicoding/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/storyappdicoding/ui/detail/DetailActivity.kt
@@ -6,16 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.storyappdicoding.R
+import com.example.storyappdicoding.databinding.ActivityDetailBinding
+import com.example.storyappdicoding.pref.SessionManager
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+    private lateinit var sessionManager: SessionManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
+        setContentView(binding.root)
+
+        sessionManager = SessionManager(this)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val storyId = intent.getStringExtra("STORY_ID")
+        val token = sessionManager.getAuthToken()
+        binding.storyId.text = "your id is $storyId"
     }
 }
